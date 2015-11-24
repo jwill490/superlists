9a6e323853fe16bd5f69e5d1ea8c65ad9e9d396e
John Williams <JMacBook@Johns-MBP.att.net>
2015-11-24 13:43:03 -0500

    Three Strikes and Refactor

--- functional_tests.py
+++ functional_tests.py
@@ -1,3 +1,4 @@
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
@@ -6,7 +7,7 @@ class NewVisitorTest(unittest.TestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(5)
+		self.browser.implicitly_wait(4)
 
 	def tearDown(self):
 		self.browser.quit()
@@ -16,7 +17,7 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.get('http://localhost:8000')
 		#She notices the page title and header mentions to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		header_text = self.browser.find_element_by_tag_name('h1').header_text
+		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
 		#She is invited to enter a to-do item straight away
 		inputbox = self.browser.find_element_by_id('id_new_item')
@@ -30,13 +31,25 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 
 		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find.elements_by_tage_name('tr')
-		self.assertTrue(any(row.text == '1: Buy peacock feathers' for rows in rows))
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
 		#There is still a text box inviting her to add another item.
 		#She enters "Use peacoc feathers to make a fly"
-		self.fail('Finish the test!')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
 		#The page updates again, and now shows both items on her list
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		#Edith wonders whether the site will remember her list. Then she sees that 
+		#that the site has generated a unique URl for her -- there is some
+		#explanatory text to that effect
+		self.fail('Finish the test!')
+		#She visits that URL - her to-do list is still there.
+
 #Checks to see if it's been executed from the commandl ine
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
